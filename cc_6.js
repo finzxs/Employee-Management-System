8ec8cfe26ed37a202b7b1640c5bead2e5fde04f1
Hayden <haydenfinney@usf.edu>
2025-10-01 14:07:43 -0400

    Added sample employees and managers using New employee() and New manager()

--- cc_6.js
+++ cc_6.js
@@ -22,9 +22,36 @@ class Manager extends Employee {
   };
 };
 //Step 4: sample employees and managers (use New employee() and New manager())
+const mgr = new Manager("Sonia Patel", "Marketing", 8);
+const emp = new Employee("matthew Smith", "Marketing");
+const emp2 = new Employee("Hillary Davis", "IT");
 
+console.log(mgr.describe());
+console.log(emp.describe());
+console.log(emp2.describe());
 //Step 5: creat a company class that holds employees and managers in an array
-
+class Company {
+    constructor() {
+        this.employees = [];
+        this.managers = [];
+    };
+    addEmployee(employee) {
+        this.employees.push(employee);
+    };
+    addManager(manager) {
+        this.managers.push(manager);
+    };
+    listManagers() {
+        this.managers.forEach(manager => {
+            console.log(manager.describe());
+        });
+    };
+    listEmployees() {
+        this.employees.forEach(emp => {
+            console.log(emp.describe());
+        });
+    };
+};
 //Step 6: Instantiate company class and add employees and managers to it
 
 
